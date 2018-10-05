cd46c362882c49d3c3649d08835550c34f9194a4
Jonathan Dalpiaz <dalpiazj@gmail.com>
2018-10-05 16:00:14 -0300

    first commit

---
+++ app/src/main/java/whatfood/whatfood/MainActivity.java
@@ -0,0 +1,13 @@
+package whatfood.whatfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

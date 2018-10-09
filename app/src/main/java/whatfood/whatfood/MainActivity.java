e06271ccbe85845d27fd107a331a59596de59a1b
Jonathan Dalpiaz <dalpiazj@gmail.com>
2018-10-09 20:57:35 -0300

    alteracoes

--- app/src/main/java/whatfood/whatfood/MainActivity.java
+++ app/src/main/java/whatfood/whatfood/MainActivity.java
@@ -1,7 +1,11 @@
 package whatfood.whatfood;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exibirReceitas(View v){}
+
+    public void cadastrarIngredientes(View v){}
+
+    public void ingredientesCadastrados(View v){}
+
+    public void sairTelaInicial(View v){ this.finish();}
+
+    private AlertDialog alerta;
+
+    private void mensagemSobre() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("What Food");
+
+        builder.setMessage("Aplicativo desenvolvido na disciplina de programação para dispositivos móveis. " +
+                           "O aplicativo tem como objetivo sugerir receitas a partir de ingredientes cadastrados pelo usuário. " +
+                           "Acadêmicos desenvolvedores: Eduardo Ludwig e Jonathan Dalpiaz");
+
+        builder.setPositiveButton("Fechar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+            }
+        });
+        alerta = builder.create();
+        alerta.show();
+    }
+    public void sobreAplicativo(View v){
+        mensagemSobre();
     }
 }

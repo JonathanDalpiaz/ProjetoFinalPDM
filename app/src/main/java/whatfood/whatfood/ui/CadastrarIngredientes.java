009ae81bf1a0674518219ddc630a26d8c1d54cd1
Jonathan Dalpiaz <dalpiazj@gmail.com>
2018-10-09 21:17:44 -0300

    alteracoes

---
+++ app/src/main/java/whatfood/whatfood/ui/CadastrarIngredientes.java
@@ -0,0 +1,15 @@
+package whatfood.whatfood.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import whatfood.whatfood.R;
+
+public class CadastrarIngredientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_ingredientes);
+    }
+}

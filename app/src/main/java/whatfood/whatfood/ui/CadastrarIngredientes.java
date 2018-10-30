bc9a4d01592ea262c5f2f8b326ff351cce468151
Jonathan Dalpiaz <dalpiazj@gmail.com>
2018-10-30 15:48:48 -0300

    alteracoes

--- app/src/main/java/whatfood/whatfood/ui/CadastrarIngredientes.java
+++ app/src/main/java/whatfood/whatfood/ui/CadastrarIngredientes.java
@@ -3,16 +3,28 @@ package whatfood.whatfood.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 import whatfood.whatfood.R;
 
 public class CadastrarIngredientes extends AppCompatActivity {
 
+    EditText edtNomeIngrediente;
+    EditText edtQuantidade;
+    Spinner spnMedidaQuantidade;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_ingredientes);
+
+        edtNomeIngrediente = (EditText)findViewById(R.id.edtNomeIngrediente);
+        edtQuantidade = (EditText)findViewById(R.id.edtQuantidade);
+        spnMedidaQuantidade = (Spinner)findViewById(R.id.spnMedidaQuantidade);
     }
+
 
     public void gravarIngrediente(View v){
 

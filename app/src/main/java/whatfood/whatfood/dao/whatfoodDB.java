9f878c25aedbbf7d181609dd3951a56a51b89bdb
Jonathan Dalpiaz <dalpiazj@gmail.com>
2018-10-30 15:07:46 -0300

    alteracoes

---
+++ app/src/main/java/whatfood/whatfood/dao/whatfoodDB.java
@@ -0,0 +1,18 @@
+package whatfood.whatfood.dao;
+
+public class whatfoodDB {
+
+    tabela ingredientesUsuario
+    coluna nomeIngrediente
+    coluna quantidade
+    coluna medidaQuantidade
+
+
+
+    tabela ingredientesReceita
+    coluna nomeIngrediente
+    coluna quantidade
+    coluna medidaQuantidade
+
+
+}
--- app/src/main/java/whatfood/whatfood/ui/CadastrarIngredientes.java
+++ app/src/main/java/whatfood/whatfood/ui/CadastrarIngredientes.java
@@ -3,7 +3,7 @@ package whatfood.whatfood.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Toast;
 import whatfood.whatfood.R;
 
 public class CadastrarIngredientes extends AppCompatActivity {
@@ -14,7 +14,11 @@ public class CadastrarIngredientes extends AppCompatActivity {
         setContentView(R.layout.activity_cadastrar_ingredientes);
     }
 
-    public void gravarIngrediente(View v){}
+    public void gravarIngrediente(View v){
+
+        Toast.makeText(getApplicationContext(), "Ingrediente cadastrado com sucesso !", Toast.LENGTH_SHORT).show();
+
+    }
 
     public void voltarTelaInicial(View v){
 

cd5b82d9c24e8786734734a6cf6e321b36d281f2
Jonathan Dalpiaz <dalpiazj@gmail.com>
2018-11-26 12:02:57 -0200

    alteracoes

--- app/src/main/java/whatfood/whatfood/dao/whatfoodDB.java
+++ app/src/main/java/whatfood/whatfood/dao/whatfoodDB.java
@@ -2,17 +2,17 @@ package whatfood.whatfood.dao;
 
 public class whatfoodDB {
 
-    tabela ingredientesUsuario
-    coluna nomeIngrediente
-    coluna quantidade
-    coluna medidaQuantidade
+   // tabela ingredientesUsuario
+   // coluna nomeIngrediente
+   //coluna quantidade
+    //coluna medidaQuantidade
 
 
 
-    tabela ingredientesReceita
-    coluna nomeIngrediente
-    coluna quantidade
-    coluna medidaQuantidade
+    //tabela ingredientesReceita
+    //coluna nomeIngrediente
+    //coluna quantidade
+    //coluna medidaQuantidade
 
 
 }
